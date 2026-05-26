72c3d932f980f4880c939362af8b764d527e08f5
Luis <luisfelipesn@hotmail.com>
2026-05-25 20:42:51 -0400

    REFACTOR: adiciona validacoes na transferencia

--- banco/transferencia.py
+++ banco/transferencia.py
@@ -6,5 +6,23 @@ class Transferencia:
         self.valor_transacao = valor_transacao
 
     def executar(self):
+
+        self._validar_contas()
+        self._validar_valor()
+
         self.conta_origem.sacar(self.valor_transacao)
         self.conta_destino.depositar(self.valor_transacao)
+
+    def _validar_contas(self):
+
+        if self.conta_origem == self.conta_destino:
+            raise ValueError(
+                "Conta origem e destino não podem ser iguais"
+            )
+
+    def _validar_valor(self):
+
+        if self.valor_transacao <= 0:
+            raise ValueError(
+                "Valor da transferência deve ser positivo"
+            )
